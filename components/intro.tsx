15cb2bc1d21a4e79e57efb9831f134fcc22a0b24
Prathamesh Kulkarni <pratham.kulkarni1908@gmail.com>
2025-08-11 15:26:35 +0200

    resume url change

--- components/intro.tsx
+++ components/intro.tsx
@@ -92,7 +92,7 @@ export default function Intro() {
 
 				<a
 					className="group bg-white px-7 py-3 flex items-center gap-2 rounded-full outline-none focus:scale-110 hover:scale-110 active:scale-105 transition cursor-pointer border border-black/10 dark:bg-white/10"
-					href="/Prathamesh-Kulkarni-Resume-2024.pdf"
+					href="/Prathamesh-Kulkarni-Resume.pdf"
 					download
 				>
 					Download CV{" "}
